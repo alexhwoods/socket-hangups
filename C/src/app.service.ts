1292ebb69df88b36d08ef061e604de1e9b29ccbd
Alex Woods <alexhwoods@gmail.com>
2021-01-16 15:06:55 -0500

    Add endpoint to C

--- C/src/app.service.ts
+++ C/src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private httpService: HttpService) {}
+
+  async getHello(): Promise<string> {
+    const { data } = await this.httpService
+      .get('http://localhost:3302/')
+      .toPromise();
+
+    return data + '\n(this written from C)';
   }
 }
