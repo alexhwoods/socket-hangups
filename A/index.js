5938d1e79e7c0de894beca7425825c927c77dd40
Alex Woods <alexhwoods@gmail.com>
2021-01-16 14:27:35 -0500

    Complete A

---
+++ A/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3301
+
+app.get('/', (req, res) => {
+  setTimeout(() => res.send('Hello World!\n'), 10 * 1000)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
