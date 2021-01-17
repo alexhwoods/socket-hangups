5ba77894ccd4b4ca7277cb5b62a776b80be78f69
Alex Woods <alexhwoods@gmail.com>
2021-01-17 12:02:32 -0500

    Make C-native-code and C equivalent

--- C/src/app.service.ts
+++ C/src/app.service.ts
@@ -4,6 +4,8 @@ import { ConfigurationService } from './configuration/configuration.service';
 import * as util from 'util';
 import { AxiosError, AxiosRequestConfig } from 'axios';
 
+let count = 0;
+
 @Injectable()
 export class AppService {
   constructor(
@@ -12,38 +14,29 @@ export class AppService {
   ) {}
 
   async getHello(): Promise<string> {
+    count++;;
+    console.log(
+      `${this.formatDate(new Date())} | received request ${count}`,
+    );
+
     await this.httpService
       .get('http://localhost:3302')
       // .get('https://google.com')
       .toPromise()
-      // .then(value => {
-      //   console.log(JSON.stringify(value.headers));
-      // })
+      .then(value => {
+        console.log('finished')
+      })
       .catch(err => {
         if (err.isAxiosError) {
           const error = err as AxiosError;
-
-          // console.log(`httpsAgent: ${error.config.httpsAgent}`);
-          console.log(`request: ${util.inspect(error.request)}`);
-// 
-          console.log(`reusedSocket: ${error.request.reusedSocket}`);
-          console.log(`response: ${util.inspect(error.response)}`);;
-
-          process.exit(1);
+          console.log(error.message)
         }
       });
 
     return 'fpp';
   }
 
-  // @Cron('* * * * * *')
-  // handleCron() {
-  //   const x = this.httpService.axiosRef.toString();
-  //   const a = this.configurationService.httpAgent.getCurrentStatus(); // I think this is a different instance of configuration service, created by Nest DI
-  //   const b = this.configurationService.httpsAgent.getCurrentStatus();
-
-  //   console.log(`a: ${JSON.stringify(a)}`);
-  //   console.log(`b: ${JSON.stringify(b)}`);
-  //   console.log(`x: ${x}`);
-  // }
+  formatDate(date) {
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  }
 }
