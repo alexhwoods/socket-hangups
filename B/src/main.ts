d13dcc8612fc03cf2e4d49773f535a53106edc04
Alex Woods <alexhwoods@gmail.com>
2021-01-18 14:18:01 -0500

    Disable keepAliveTimeout

--- B/src/main.ts
+++ B/src/main.ts
@@ -4,14 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const httpAdapter = app.getHttpAdapter();
+  // const httpAdapter = app.getHttpAdapter();
 
-  const server = httpAdapter.getHttpServer() as Server;
-  server.keepAliveTimeout = 30000;
+  // const server = httpAdapter.getHttpServer() as Server;
+  // server.keepAliveTimeout = 30000;
 
-  console.log(JSON.stringify(server));
-
-
+  // console.log(JSON.stringify(server));
   await app.listen(4002);
 }
 bootstrap();
